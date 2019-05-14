4b6fd662897ebbebd231073820f89c1360870552
PascalUlor <pascalulor@yahoo.com>
2019-05-14 13:54:35 +0100

    ft(add mouseover event to images add image zoom style to home-page.less file create javascript code to loop through all images and add img-zoom class to their parent element

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+/**
+ * Mouseover Anmation
+ */
+const imageArray = document.images;
+
+[...imageArray].forEach(image => {
+    const item = document.querySelector('.'+image.className);
+    item.addEventListener('mouseover', (e) =>{
+        e.preventDefault();
+        document.querySelector('.'+image.className).parentNode.classList.add("img-zoom"); 
+    })
+});
